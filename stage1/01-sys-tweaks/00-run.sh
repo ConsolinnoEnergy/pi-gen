af0f8f12fc40527ddf39f862f754b6eea6fd9d87
Leonid Verhovskij <l.verhovskij@gmail.com>
2018-10-09 14:27:32 +0200

    use default password from file if given, remove noobs img creation

--- stage1/01-sys-tweaks/00-run.sh
+++ stage1/01-sys-tweaks/00-run.sh
@@ -4,12 +4,16 @@ install -d "${ROOTFS_DIR}/etc/systemd/system/getty@tty1.service.d"
 install -m 644 files/noclear.conf "${ROOTFS_DIR}/etc/systemd/system/getty@tty1.service.d/noclear.conf"
 install -v -m 644 files/fstab "${ROOTFS_DIR}/etc/fstab"
 
+if [ -f files/pi_default_pass ]; then
+	install -v -m 644 files/pi_default_pass "${ROOTFS_DIR}/tmp/pi_default_pass"
+else
+	echo "pi:raspberry" > "${ROOTFS_DIR}/tmp/pi_default_pass"
+fi
+
 on_chroot << EOF
 if ! id -u pi >/dev/null 2>&1; then
 	adduser --disabled-password --gecos "" pi
 fi
-echo "pi:raspberry" | chpasswd
+cat /tmp/pi_default_pass | chpasswd
 echo "root:root" | chpasswd
 EOF
-
-
